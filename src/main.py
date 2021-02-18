c62562b9ecea834cfae6ab0f38182f72e2d58606
Lorenzo <brainstew927@gmail.com>
2021-02-18 22:43:58 +0100

    commit with the reading of the api key from the file

--- src/main.py
+++ src/main.py
@@ -1,34 +1,33 @@
 import tkinter as tk
-from tkinter.constants import RIGHT
+
+
+class NASA_API():
+    def __init__(self, file_path="./key"):
+        
+        # read the api from the file
+        self.api_key = ""
+        with open(file_path,"r") as f:
+            try:
+                self.api_key = f.readline()
+            except FileNotFoundError:
+                print("ERROR: file for the API key was not found")
+                quit()
+        # if the api_key is empty then quit
+        if(self.api_key == ""):
+            print("ERROR: couldn't get the API key")
+            quit()
+
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-class Application_prova(tk.Frame):
+    # constructor for the class
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
         self.create_widg()
-
+    # function used to create the widgets and putting them into the frame
     def create_widg(self):
+
         # button to get the picture
         self.get_photo_button = tk.Button(self)
         self.get_photo_button["text"] = "get the Picture"
@@ -40,9 +39,8 @@ class Application_prova(tk.Frame):
         self.save_photo_button["text"] = "save the photo"
         self.save_photo_button["state"] = "disabled"
         self.save_photo_button.pack(side="right")
-    
 
-
+ap = NASA_API() 
 root = tk.Tk()
-app = Application_prova(master=root)
+app = Application(master=root)
 app.mainloop()
