ceac56a12ebb04737fe6cc9172a42225550f6198
Lorenzo <brainstew927@gmail.com>
2021-02-21 17:57:07 +0100

    added resizing and icon

--- src/application.py
+++ src/application.py
@@ -1,11 +1,20 @@
-from os import system
-from tkinter.constants import ACTIVE, DISABLED, LEFT, NORMAL
+from tkinter.constants import DISABLED, LEFT, NORMAL, YES
 import PIL.Image
 import PIL.ImageTk
+import PIL.ImageOps
 import tkinter as tk
 from nasa_api import nasa_api
 
 class Application(tk.Frame):
+    # event called when the window is resized
+    def _resize(self, event):
+        if event == tk.EventType.ResizeRequest:
+            self._resize_img(event.width, event.height)
+    def _resize_img(self, width, height):
+        # self.im = self.im.resize((width, height))
+        self.im = PIL.ImageOps.fit(self.original_image, (width, height))
+
+        self.change_img()
     def change_img(self):
         # create a PhotoImage obj from the im returned by the api
         self.img = PIL.ImageTk.PhotoImage(self.im)
@@ -23,7 +32,7 @@ class Application(tk.Frame):
         # create the label which will be used to display the image
         self.lab = tk.Label(self)
         # pack the label
-        self.lab.pack()
+        self.lab.pack(expand=YES)
         # pack the window
         self.pack()
     def save_image(self):
@@ -37,7 +46,8 @@ class Application(tk.Frame):
         # get the image link from the api
         link = self.api.get_photo_link()
         # open the image using PIL.Image
-        self.im = PIL.Image.open(self.api.get_stream_from_link(link=link))
+        self.original_image = PIL.Image.open(self.api.get_stream_from_link(link=link))
+        self.im = self.original_image
         self.im_name = self.api.get_photo_title()
         # display the image
         self.change_img()
@@ -45,8 +55,14 @@ class Application(tk.Frame):
         self.save["state"] = NORMAL
     def __init__(self, master):
         tk.Frame.__init__(self, master=master)
+        master.title("APOC")
+        # set the icon
+        self.icon = tk.PhotoImage(file = '.\\icon.png')
+        master.iconphoto(False, self.icon)
         # initialise the nasa api object
         self.api = nasa_api()
         self.master = master
+        self.im = None
         # create all the widgets which will be used
         self.create_widget()
+        self.bind("<Configure>",self._resize)
